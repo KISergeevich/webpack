ba973be6d8ae6c99b59e2b6fb6595743b2ad2ea1
Kalinin Ivan <indimion57@gmail.com>
2022-11-08 18:59:01 +0300

    react and typescript

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,10 +1,10 @@
 import * as $ from 'jquery'
 
-function createAnalytics() {
+function createAnalytics(): object {
     let counter = 0;
-    let destroyed = false;
+    let destroyed: boolean = false;
 
-    const listener = () => counter++;
+    const listener = (): number => counter++;
 
     $(document).on('click', listener)
 
@@ -23,5 +23,5 @@ function createAnalytics() {
     }
 }
 
-window.analytics = createAnalytics();
+window['analytics'] = createAnalytics();
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,16 @@ import Post from '@models/Post';
 // import xml from './assets/data.xml';
 // import csv from './assets/data.csv';
 import WebpackLogo from '@/assets/webpack-logo.png';
+import React from 'react';
+import {render} from 'react-dom';
 import './styles/styles.css';
 import './styles/less.less';
 import './babel.js'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
-$('pre').addClass('code').html(post.toString())
+//$('pre').addClass('code').html(post.toString())
+
 
 
 // console.log('JSON:', json)
